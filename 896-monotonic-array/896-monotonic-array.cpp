5c7946dff6570305e2d4d61bc5062411f8058a61
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-06-21 21:31:39 +0100

    Time: 184 ms (88.55%), Space: 96.5 MB (64.51%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -5,13 +5,8 @@ public:
         int i = 0;
         while (i < length)
         {
-            cout << "the numbers are :" << nums[i] << " and " << nums[i + 1] << endl; 
             if (nums[i] > nums[i + 1])
-            {
-                cout << "HERE" << endl;
                 return (false);
-            }
-                
             i++;
         }
         return (true);
@@ -23,10 +18,7 @@ public:
         while (i < length)
         {
             if (nums[i] < nums[i + 1])
-            {
-                cout << "HERE" << endl;
                 return (false);
-            }
             i++;
         }
         return (true);
