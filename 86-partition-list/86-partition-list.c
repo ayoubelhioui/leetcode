be2b90d924d66187b3dc608f117f877467c7c20e
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-07-22 06:36:52 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (52.53%) - LeetHub

--- 86-partition-list/86-partition-list.c
+++ 86-partition-list/86-partition-list.c
@@ -39,15 +39,7 @@ void fill_arrays(int *small, int *big, int size)
         i++;
     }
 }
-void from_array_to_list(struct ListNode *temp, int *array, int size, int *i)
-{
-    while (*i < size && array[*i] != -200)
-    {
-        temp->val = array[*i];
-        temp = temp->next;
-        *i = *i + 1;
-    }  
-}
+
 struct ListNode* partition(struct ListNode* head, int x){
     int i = 0;
     int size = get_size(head);
