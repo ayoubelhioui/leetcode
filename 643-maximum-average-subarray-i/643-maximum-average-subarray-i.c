48b51258e4a6f2c0e62fd62b2dc7680e821e3ad8
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-07-31 10:53:55 -0700

    Time: 276 ms (45.00%), Space: 14.2 MB (27.50%) - LeetHub

---
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.c
@@ -0,0 +1,18 @@
+
+
+#define MAX(x, y)(x > y ? x : y);
+double findMaxAverage(int* nums, int numsSize, int k){
+    double result = 0, sum = 0;
+    int i = 0;
+    while (i < k)
+        sum += nums[i++];
+    sum = sum;
+    result = sum;
+    while (i < numsSize)
+    {
+        sum += nums[i] - nums[i - k];
+        result = MAX(sum, result);
+        i++;
+    }
+    return (result / k);
+}
