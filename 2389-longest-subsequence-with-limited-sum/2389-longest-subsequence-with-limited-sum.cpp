e9984540204383eaf982bf7228bb57713e3dcda2
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-10-19 10:55:35 +0100

    Time: 49 ms (51.92%), Space: 13.7 MB (82.09%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
+    int binarySearch(vector<int> &nums, int target, int size){
+        int start = 0, end = size - 1, med = 0;
+        while (start <= end)
+        {
+            med = (start + end) / 2;
+            // printf("med is : %d\n", med);
+            if (nums[med] > target)
+                end = med - 1;
+            else if (nums[med] < target)
+            {
+                if (med < size - 1 && nums[med + 1] > target)
+                {
+                    // printf("here\n");
+                    if (med == size - 1)
+                        return (med + 2);
+                    return (med + 1);                    
+                }
+                else
+                    start = med + 1;
+            }
+            else
+                return (med + 1);
+        }
+        return (start);
+    }
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(), nums.end());
         int sum = 0, queriesSize = queries.size(), numsSize = nums.size(), answer = 0;
         vector<int> summingElements(numsSize);
+        printf("nums size is : %d\n", numsSize);
         vector<int> result(queriesSize);
-        
         for (int i = 0; i < numsSize; i++){
             sum += nums[i];
             summingElements[i] = sum;
         }
         for (int i = 0; i < queriesSize; i++){
-            answer = 0;
-            for (int j = 0; j < numsSize; j++){
-                if (queries[i] >= summingElements[j])
-                    answer++;
-                else
-                    break;
-            }
-            result[i] = answer;
+            result[i] = binarySearch(summingElements, queries[i], numsSize);
         }
         return (result);
     }
