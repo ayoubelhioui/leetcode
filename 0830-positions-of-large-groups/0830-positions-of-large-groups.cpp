4acf7c1294042d2eb0e2f6ae0de3441d0fdf0806
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-10-25 14:29:06 +0100

    Time: 11 ms (17.71%), Space: 7.3 MB (81.49%) - LeetHub

--- 0830-positions-of-large-groups/0830-positions-of-large-groups.cpp
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.cpp
@@ -2,24 +2,14 @@ class Solution {
 public:
     vector<vector<int>> largeGroupPositions(string s) {
 	vector<vector<int>> returnedArray;
-	int right = 1, left = 0, count = 0, length = s.length();
+	int length = s.length(), saver = 0;
 	for(int i = 0; i < length; i++)
 	{
-		if (i < length - 1 && s[i] == s[i + 1])
-		{
-			int saver = i;
-			while (s[i] == s[i + 1])
-				i++;
-			if (i - saver >= 2)
-			{
-				vector<int> v(2);
-				v[0] = saver;
-				v[1] = i;
-				returnedArray.push_back(v);
-			}
-			if (!s[i])
-				break ;
-		}
+        saver = i;
+        while (s[i] == s[i + 1])
+            i++;
+        if (i - saver >= 2)
+            returnedArray.push_back({saver, i});
 	}
 	return (returnedArray);
 }
