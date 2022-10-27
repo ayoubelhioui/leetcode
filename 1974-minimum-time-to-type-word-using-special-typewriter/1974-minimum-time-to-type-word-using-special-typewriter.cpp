73024ab8a613a082e623e20d7fac36b07b65e89e
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-10-27 16:08:17 +0100

    Time: 4 ms (32.58%), Space: 6.2 MB (45.57%) - LeetHub

---
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minTimeToType(string word) {
+        int wordLength = word.length(), value = 0;
+        int total = (word[0] - 'a') <= 13 ? abs(word[0] - 'a') : 26 - abs(word[0] - 'a');
+        total++;
+        for (int i = 0; i < wordLength - 1; i++)
+        {
+            value = abs(word[i + 1] - word[i]);
+            // printf("the value is : %d\n", value);
+            if (value <= 13)
+                total += value;
+            else
+                total += 26 - value;
+            total++;
+        }
+        return (total);
+    }
+};
