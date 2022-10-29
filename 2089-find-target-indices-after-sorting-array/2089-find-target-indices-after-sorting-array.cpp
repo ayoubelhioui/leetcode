456ec3b523ec3fc36fce65d98ca55248f8a99c19
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-10-29 13:04:57 +0100

    Time: 4 ms (91.30%), Space: 11.7 MB (59.31%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -21,47 +21,17 @@ public:
             }
         }
         return -1;
-    }
-    
-    int findLastOccurence(vector<int> &nums, int target, int size)
-    {
-        int start = 0, end = size - 1, med = 0;
-        while (start <= end)
-        {
-            med = (start + end) / 2;
-            if (nums[med] > target)
-                end = med - 1;
-            else if (nums[med] < target)
-                start = med + 1;
-            else
-            {
-                if (med == size - 1)
-                    return (med);
-                if (med < size && nums[med + 1] > target)
-                {
-                    return (med);
-                }
-                else if (med < size && nums[med + 1] == target)
-                {
-                    // printf("here\n");
-                    start = med + 1;
-                }
-                    
-            }
-        }
-        return -1;
-    }
-    
+    }    
     vector<int> targetIndices(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
         int size = nums.size(), firstIndex = 0, secondIndex = 0;
         vector<int> result;
         firstIndex = findFirstOccurence(nums, target, size);
-        secondIndex = findLastOccurence(nums, target, size);
-        if (firstIndex == -1)
+         if (firstIndex == -1)
             return (result);
-        while (firstIndex <= secondIndex)
-            result.push_back(firstIndex++);
+        secondIndex = firstIndex;
+        while (secondIndex < size && nums[secondIndex] == nums[firstIndex])
+            result.push_back(secondIndex++);
         return (result);
     }
 };
