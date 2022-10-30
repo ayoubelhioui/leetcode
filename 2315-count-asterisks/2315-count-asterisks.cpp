1d899b2ca47e8a282eb2dbdde7fd6e5941aef197
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-10-30 08:48:51 +0100

    Time: 7 ms (20.07%), Space: 6.4 MB (73.47%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -0,0 +1,19 @@
+#define ASTERISK '*'
+#define VERTICAL_BAR '|'
+class Solution {
+public:
+    
+    int countAsterisks(string s) {
+        int sLength = s.length(), flag = 0, asteriskCounter = 0;
+        for (auto character : s)
+        {
+            if (character == VERTICAL_BAR)
+            {
+                (flag == 0) ? flag = 1 : flag = 0;
+            }
+            else if (character == ASTERISK && flag == 0)
+                asteriskCounter++;
+        }
+        return (asteriskCounter);
+    }
+};
