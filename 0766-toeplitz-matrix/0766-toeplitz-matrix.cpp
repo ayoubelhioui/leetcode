6c3da67687650e5bd3836d2e67dd015010de4126
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-10-31 07:49:35 +0100

    Time: 39 ms (21.35%), Space: 17.5 MB (15.16%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool DFS(vector<vector<int>> &matrix, int i, int j, int matrixSize, int value)
+    {
+        if (i == matrixSize || j == matrix[i].size() || matrix[i][j] == -1)
+            return (true);
+        printf("it is : %d, and the value is : %d\n", matrix[i][j], value);
+        if (matrix[i][j] != value)
+            return (false);
+        if (!DFS(matrix, i + 1, j + 1, matrixSize, matrix[i][j]))
+            return (false);
+        matrix[i][j] = -1;
+        return (true);
+    }
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        
+        int matrixSize = matrix.size();
+        for (int i = 0; i < matrixSize; i++)
+        {
+            for (int j = 0; j < matrix[i].size(); j++)
+                if (!DFS(matrix, i, j, matrix.size(), matrix[i][j]))
+                    return (false);
+        }
+        return (true);
+    }
+};
