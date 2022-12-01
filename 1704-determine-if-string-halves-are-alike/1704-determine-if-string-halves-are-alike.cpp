081f3f44b46a92c6f55f72e5f927dfc2740de811
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-12-01 08:01:35 +0100

    Time: 9 ms (28.24%), Space: 6.5 MB (83.41%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -4,10 +4,7 @@ public:
         int count = 0, i = -1, j = s.length();
         string vowels = "aeiouAEIOU";
         while (++i < --j)
-        {
-            count += (vowels.find(s[i]) > 10);
-            count -= (vowels.find(s[j]) > 10);
-        }
+            count += (vowels.find(s[i]) > 10) - (vowels.find(s[j]) > 10);
         return (count == 0);
     }
 };
