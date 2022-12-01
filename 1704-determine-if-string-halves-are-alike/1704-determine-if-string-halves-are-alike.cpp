ca2f36c16c1c9c006f9b12e794b776660cbfdcb7
ayoubelhioui <73433287+ayoubelhioui@users.noreply.github.com>
2022-12-01 08:00:39 +0100

    Time: 3 ms (87.76%), Space: 6.7 MB (56.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-        int count = 0, i = 0;
-        for (i = 0; i < s.length() / 2; i++)
-            if (s[i] == 'e' || s[i] == 'E' || s[i] == 'a' || s[i] == 'A' || s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' || s[i] == 'u' || s[i] == 'U')
-                count++;
-        while (i < s.length())
+        int count = 0, i = -1, j = s.length();
+        string vowels = "aeiouAEIOU";
+        while (++i < --j)
         {
-            if (s[i] == 'e' || s[i] == 'E' || s[i] == 'a' || s[i] == 'A' || s[i] == 'i' || s[i] == 'I' || s[i] == 'o' || s[i] == 'O' || s[i] == 'u' || s[i] == 'U')
-                count--;
-            i++;
+            count += (vowels.find(s[i]) > 10);
+            count -= (vowels.find(s[j]) > 10);
         }
         return (count == 0);
     }
